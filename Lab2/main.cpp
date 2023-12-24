deb02f93de660ae2bc086a177e909953b5655793
Auxman-98 <138567488+Auxman-98@users.noreply.github.com>
2023-12-24 19:32:40 +0200

    Update main.cpp

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -12,7 +12,7 @@ int main()
 	Vector3d<double> vec3db = Vector3d<double>(31, 96, 60);
 	vec3db.print3dVec();
 	Point2d<float> sp = Point2d<float>(3, 7);
-	std::cout << "{ " << sp.x << " " << sp.y << " }\n";
+	std::cout << "(" << sp.x << " " << sp.y << ")\n";
 	Vector3d<float> simpleVec = Vector3d<float>(sqrt(2), sqrt(3), sqrt(5));
 	simpleVec.print3dVec();
 	system("pause");
