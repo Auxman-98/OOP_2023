fe5611ad02388b69e373ac22acdee3df1ee81a7e
Auxman-98 <138567488+Auxman-98@users.noreply.github.com>
2024-02-01 22:39:24 +0200

    Update main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -9,54 +9,61 @@ int main()
 	for (int k = 0; k < 2; k++) {
 		for (int j = 0; j < 2; j++) {
 			for (int i = 0; i < 2; i++) {
-				myArray[i][j][k] = (i*2 + j)*2 + k;
+				myArray.index(i, j, k) = (i*2 + j)*2 + k;
 			}
 		}
 	}
 
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			for (int k = 0; k < 2; k++) {
-				std::cout << myArray[i][j][k] << "\t";
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				std::cout << myArray.index(i, j, k) << "\t";
 			}
 			std::cout << std::endl;
 		}
 		std::cout << std::endl;
 		std::cout << std::endl;
 	}
-	
-	std::cout << "Ñðåç ìàññèâà ïî êîîðäèíàòå x=1" << std::endl;
-	Array3d<int>::Submatrix slice0d1 = myArray.GetValues0(1);
-	for (int j = 0; j < 2; j++) {
-		for (int i = 0; i < 2; i++) {
-			std::cout << slice0d1[j][i] << "\t";
+
+	std::cout << "Срез массива по координате x=1" << std::endl;
+	int* slice0d1 = new int[2 * 2];
+	slice0d1 = myArray.GetValues0(1);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			std::cout << slice0d1[i * 2 + j] << "\t";
 		}
 		std::cout << std::endl;
 	}
+	delete[] slice0d1;
 	std::cout << std::endl;
-	std::cout << "Ñðåç ìàññèâà ïî êîîðäèíàòàì x=0 è y=0" << std::endl;
-	Array3d<int>::Submatrix::Element slice01d00 = myArray.GetValues01(0, 0);
+	std::cout << "Срез массива по координатам x=0 и y=0" << std::endl;
+	int* slice01d00 = new int[2];
+	slice01d00 = myArray.GetValues01(0, 0);
 	for (int i = 0; i < 2; i++) {
 		std::cout << slice01d00[i] << "\t";
 	}
+	delete[] slice01d00;
 	std::cout << std::endl;
 	std::cout << std::endl;
-	
-	std::cout << "Íîâûå ýëåìåíòû ñ êîîðäèíàòîé x=1" << std::endl;
+
+
+	std::cout << "Новые элементы с координатой x=1" << std::endl;
 	myArray.SetValues0(1);
 	for (int k = 0; k < 2; k++) {
 		for (int j = 0; j < 2; j++) {
-			std::cout << myArray[k][j][1] << "\t";
+			std::cout << myArray.index(k, j, 1) << "\t";
 		}
 		std::cout << std::endl;
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
-
-	std::cout << "Íîâûå ýëåìåíòû ñ êîîðäèíàòàìè y=1 è z=0" << std::endl;
+	std::cout << "Новые элементы с координатами y=1 и z=0" << std::endl;
 	myArray.SetValues12(1, 0);
 	for (int i = 0; i < 2; i++) {
-		std::cout << myArray[0][1][i] << "\t";
+		std::cout << myArray.index(0, 1, i) << "\t";
 	}
 	std::cout << std::endl;
 	std::cout << std::endl;
@@ -67,7 +74,7 @@ int main()
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
 			for (int k = 0; k < 2; k++) {
-				std::cout << myArray[i][j][k] << "\t";
+				std::cout << myArray.index(i, j, k) << "\t";
 			}
 			std::cout << std::endl; 
 		}
@@ -75,13 +82,13 @@ int main()
 		std::cout << std::endl; 
 	}
 
-	std::cout << "Ïðîâåðêà âûçîâà ìåòîäà np.zeros" << std::endl;
-	myArray.numFill(0);
+	std::cout << "Проверка вызова метода np.zeros" << std::endl;
 	system("pause");
-        for (int i = 0; i < 2; i++) {
+	myArray.numFill(0);
+	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 2; j++) {
 			for (int k = 0; k < 2; k++) {
-				std::cout << myArray[i][j][k] << "\t";
+				std::cout << myArray.index(i, j, k) << "\t";
 			}
 			std::cout << std::endl;
 		}
