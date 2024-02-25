bb8b7de476c877c1eefe3f531e05cdd40bf6f847
Auxman-98 <138567488+Auxman-98@users.noreply.github.com>
2024-02-25 20:06:42 +0200

    Add files via upload

---
+++ Lab6/src/controls/MacOS/MacOSForm.cpp
@@ -0,0 +1,17 @@
+#include "MacOSForm.h"
+
+#include <iostream>
+
+using namespace factories;
+
+void MacOSForm::setPosition() {
+	std::cout << "Вызван метод setPosition у контролла MacOSForm" << std::endl;
+}
+
+void MacOSForm::getPosition() {
+	std::cout << "Вызван метод getPosition у контролла MacOSForm" << std::endl;
+}
+
+void MacOSForm::addControl() {
+	std::cout << "Вызван метод addControl у контролла MacOSForm" << std::endl;
+}
